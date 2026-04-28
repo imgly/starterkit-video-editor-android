8cf5f1abf282a4c89ea8e62deade0dd2c9b74e58
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-28 15:37:07 +0200

    Update starter kit for v1.74.0-rc.1

---
+++ starter-kit/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("android")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "ly.img.editor.configuration"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 36
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    api("ly.img:editor:1.74.0-rc.1")
+    // Required for enableEdgeToEdge
+    implementation("androidx.activity:activity-ktx:1.9.0")
+}
